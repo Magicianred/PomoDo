689fb20dcb68c4ac04baf1bdb44fd0de9f5f6ad4
Choppychan <s.carmen2101@gmail.com>
2019-12-03 10:42:56 +0100

    Cambiato codice per poter utilizzare più classi

--- js/cambio_stagione.js
+++ js/cambio_stagione.js
@@ -1,42 +1,42 @@
 function getSeason(d) {
   d = d || new Date();
-  var mon = d.getMonth() + 1;
-  var day = d.getDate();
-  var seasons = ['summer','fall','winter','spring', 'christmas'];
+  let mon = d.getMonth() + 1;
+  let day = d.getDate();
 
   //Winter
   if (mon >= 1 && mon <= 2) {
-    return 'winter';
+    return ['winter', 'red-text'];
   }
 
   //Spring
   else if (mon >= 2 && mon <= 6) {
-    return 'spring';
+    return ['spring'];
   }
 
   //Summer
   else if (mon >= 6 && mon <= 8) {
-    return 'summer';
+    return ['summer'];
   }
 
 
   //Fall
   else if (mon >= 9 && mon <= 11) {
-    return 'fall';
+    return ['fall'];
   }
 
   //Winter+Christmas
   else if (mon == 12) {
     if (day >= 1 && day <= 7) {
-      return 'winter';
+      return ['winter', 'red-text'];
     }
     else if (day >= 8 && day <= 31) {
-      return 'christmas';
+      return ['christmas', 'yellow-text'];
     }
   }
 
-  mon = Math.floor((mon % 12) / 3);
-  return seasons[mon];
 }
 
-document.body.classList.add(getSeason())
+let seasonClasses = getSeason();
+seasonClasses.forEach(function(seasonClass){
+	document.body.classList.add(seasonClass)
+});
