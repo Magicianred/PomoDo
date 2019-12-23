16b801d80d483b4b274bb2f39163c58343b3939a
Choppychan <s.carmen2101@gmail.com>
2019-12-23 09:42:13 +0100

    Creato nuovo file calendar.js

---
+++ js/calendar.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
