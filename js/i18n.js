5988120f6e7bd7d3e5dac2723f4b37ee95c22663
Choppychan <s.carmen2101@gmail.com>
2020-01-10 22:22:13 +0100

    Aggiunta la lingua italiana e inglese

--- js/i18n.js
+++ js/i18n.js
@@ -1,26 +1,12 @@
 $(function(){
   const i18n = {
-    'it': {
-      //Navbar and Sidenav
-      'habits': 'Abitudini',
-      'calendar': 'Calendario',
-      //PomoDo
-      'objective' : 'Scrivi il tuo obiettivo...', //??? Come si mette il placeholder?
-      'changes' : 'I cambiamenti in questa sezione si rifletteranno all\'inizio della prossima sessione di lavoro/pausa.',
-      'min-work' : 'Minuti di lavoro',
-      'min-break' : 'Minuti di pausa',
-      'session' : 'Le tue sessioni',
-      //Footer
-      'create' : 'Creato da ',
-      'design' : 'Design di ',
-      'font' : 'Font di ',
-    },
+    'it': {},
     'en': {
       // Navbar and Sidenav
       'habits': 'Habits',
       'calendar': 'Calendar',
       //PomoDo
-      'objective' : 'Write there your objective',
+      'objective' : 'Write here your objective...',
       'changes' : 'Changes made here will reflect at the start of the next work/break session.',
       'min-work' : 'Work Duration',
       'min-break' : 'Break Duration',
@@ -36,14 +22,30 @@ $(function(){
     let translation = i18n[lang];
     Object.entries(translation).forEach(function(entry){
       document.querySelectorAll('.i18n-' + entry[0]).forEach(function(el){
-        el.firstChild.textContent = entry[1].toString();
+        if(el.hasChildNodes()){
+          el.firstChild.textContent = entry[1].toString();
+        } else {
+          $(el).attr('placeholder', entry[1].toString());
+        }
       });
     });
   };
 
   (function(){
     let lang = localStorage.getItem('lang');
-    if(lang !== null && lang !== 'en' && Object.keys(i18n).includes(lang)){
+    document.querySelectorAll('[class*="i18n-"]').forEach(function(el){
+      el.classList.forEach(function(el_class){
+        if(el_class.startsWith('i18n-')){
+          if(el.hasChildNodes()){
+            i18n.it[el_class.substr(5)] = el.firstChild.textContent;
+          } else {
+            i18n.it[el_class.substr(5)] = $(el).attr('placeholder');
+          }
+          return false;
+        }
+      });
+    });
+    if(lang !== null && lang !== 'it' && Object.keys(i18n).includes(lang)){
       translate(lang);
     }
   })();
@@ -51,7 +53,7 @@ $(function(){
   $('.translate-btn').on('click', function(e){
     e.preventDefault();
     let lang = $(this).data('lang');
-    localStorage.setItem("lang", lang);
+    localStorage.setItem('lang', lang);
     translate(lang);
   })
 
